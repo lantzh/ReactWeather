be1de3545afb1e67cbbdd7ac7b524afba2e98e1a
lantzh <lantz@wiredtite.com>
2017-02-18 14:40:23 -0500

    Comment out http forwarding

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forward-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url)
-  }
-});
-
-app.use(express.static('public'));
+// app.use(function (req, res, next){
+//   if (req.headers['x-forward-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url)
+//   }
+// });
+//
+// app.use(express.static('public'));
 
 app.listen(PORT, function (){
   console.log('Express server is running on port '+ PORT);
